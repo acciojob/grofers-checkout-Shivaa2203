686354c31e15023bcd06bc962f03b9e763a407b4
Shiva Kumar Dasarla <77926770+Shivaa2203@users.noreply.github.com>
2026-05-15 11:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const getSumBtn = document.createElement("button");
+
 getSumBtn.append("Get Total Price");
+
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
- let prices = document.querySelectorAll(".price");
+
+    let prices = document.querySelectorAll(".price");
 
     let total = 0;
 
@@ -11,21 +14,27 @@ const getSum = () => {
         total += Number(item.textContent);
     });
 
+    let oldAns = document.getElementById("ans");
+
+    if (oldAns) {
+        oldAns.parentElement.remove();
+    }
+
     let table = document.querySelector("table");
 
     let newRow = document.createElement("tr");
 
     let newCell = document.createElement("td");
 
-    newCell.colSpan = 2;
+    newCell.setAttribute("colspan", "2");
+
+    newCell.id = "ans";
 
     newCell.textContent = total;
 
     newRow.appendChild(newCell);
 
     table.appendChild(newRow);
-  
 };
 
 getSumBtn.addEventListener("click", getSum);
-
