342943d1c33014f5de525ebeea11cb39a75606e2
Shiva Kumar Dasarla <77926770+Shivaa2203@users.noreply.github.com>
2026-05-15 11:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,27 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+ let prices = document.querySelectorAll(".price");
+
+    let total = 0;
+
+    prices.forEach((item) => {
+        total += Number(item.textContent);
+    });
+
+    let table = document.querySelector("table");
+
+    let newRow = document.createElement("tr");
+
+    let newCell = document.createElement("td");
+
+    newCell.colSpan = 2;
+
+    newCell.textContent = total;
+
+    newRow.appendChild(newCell);
+
+    table.appendChild(newRow);
   
 };
 
